59d71d748f1dc6792e80e66ab97e49da438fd7be
Joshua Knauber <joshua.knauber@gmail.com>
2024-01-20 13:54:49 +0100

    dismissable

--- src/components/Drawer.tsx
+++ src/components/Drawer.tsx
@@ -26,7 +26,7 @@ export function CalendarDrawer() {
       setActiveSnapPoint={setSnap}
       modal={false}
       open={true}
-      dismissible={false}
+      // dismissible={false}
     >
       <Drawer.Portal>
         <Drawer.Content className="fixed bottom-0 left-0 right-0 z-50 flex h-full flex-col rounded-t-xl bg-white text-black outline-none">
