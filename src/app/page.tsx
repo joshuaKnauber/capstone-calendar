8c774396acb7f99427d3cf0e0a15e15da81ed682
Joshua Knauber <joshua.knauber@gmail.com>
2024-01-20 14:02:01 +0100

    screen height

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { protectedServerRoute } from "@/utils/protectedServerRoute";
 export default async function Home() {
   const session = await protectedServerRoute();
   return (
-    <main className="relative min-h-screen">
+    <main className="relative min-h-[100svh]">
       <CalendarDrawer />
       <Calendar session={session} />
     </main>
